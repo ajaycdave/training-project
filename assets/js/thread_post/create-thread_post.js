819c7a5f02b1240b38211d1b535a1b91be449edd
Ajay C Dave <ajayd@aum36.aum.com>
2020-05-15 18:11:19 +0530

    Thread post valiation

--- assets/js/thread_post/create-thread_post.js
+++ assets/js/thread_post/create-thread_post.js
@@ -13,62 +13,31 @@ var KTValidationControls = function () {
         
     };
     var validationForm = function () {
-
-
-        //passwork check rule
+          //passwork check rule
         $.validator.addMethod("pwcheck", function (value) {
             return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/.test(value) // consists of only these
         });
 
         var btn = $("form :submit");
 
-        $("#threadForm").validate({
+        $("#threadpostForm").validate({
 
             ignore: '.select2-search__field,:hidden:not("textarea,.files,select")',
             // define validation rules
             rules: {
-                title:
-                {
-                    required:true
-                },
-                category_id:
-                {
-                    required:true
-                },
+                
                 description:
                 {
                     required:true
                 },
                 
-               /* vehicle_no:
-                {
-                    required:true    
-                },
-                lr_no:
-                {
-                    required:true    
-                }*/
+               
             },
             messages: {
-                title: {
-                    required: "Please enter name",
-                },
-                category_id: {
-                    required: "Please select category",
-                },
                 description: {
                     required: "Please enter description",
                 },
-                
-               /* vehicle_no: {
-                    required: "Please enter vehicle no",
-                },
-                lr_no: {
-                    required: "Please enter LR no",
-                },*/
-
-
-
+               
             },
             errorPlacement: function (error, element) {
 
