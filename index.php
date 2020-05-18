5f59e957e74c9e1d4f96abebae1bcf25f66efde4
Ajay C Dave <ajayd@aum36.aum.com>
2020-05-18 14:02:55 +0530

    Thread search changes

--- index.php
+++ index.php
@@ -43,12 +43,21 @@ class Home {
 			$search = $_REQUEST['search']['value'];
 		}
 
-		$search_string = '';
-		$objThread     = new Thread();
-		$select_query  = "SELECT th.id,title,ca.name as category_name,ca.id as category_id  FROM threads as th join category as ca on ca.id=th.category_id where 1";
-		$orderby       = "order by $order $dir";
+		/*
+		SELECT th.id,title,ca.name as category_name,ca.id as category_id,th.description,tp.description as post_message FROM threads as th
+		join category as ca on ca.id=th.category_id
+		LEFT JOIN thread_posts as tp on tp.thread_id=th.id
+		where  1 and (title like '%New thread%' or th.description like '%New thread%' or tp.description like '%New thread%')
+		GROUP by th.id
+		 */
 
-		$postscollection = $objThread->selectAll($select_query);
+		$search_string   = '';
+		$objThread       = new Thread();
+		$select_query    = "SELECT th.id,title,ca.name as category_name,ca.id as category_id,th.description,tp.description as post_message  FROM threads as th join category as ca on ca.id=th.category_id LEFT JOIN thread_posts as tp on tp.thread_id=th.id  where 1";
+		$group_by        = ' GROUP By th.id';
+		$orderby         = "order by $order $dir";
+		$base_query      = $select_query." $group_by";
+		$postscollection = $objThread->selectAll($base_query);
 
 		$totalData = count($postscollection);
 
@@ -56,15 +65,17 @@ class Home {
 			$search_string .= " and category_id ='".$category_id."'";
 		}
 		if (isset($search) && $search != "") {
-			$search_string .= " and title like '%".stripcslashes($search)."%'";
+			//$search_string .= " and title like '%".stripcslashes($search)."%'";
+			$search_string .= " and (title like '%".stripcslashes($search)."%' or th.description like '%".stripcslashes($search)."%' or tp.description like '%".stripcslashes($search)."%')";
 		}
 
 		//LIMIT 20 OFFSET 20
 
-		$select_query    = $select_query." $search_string";
+		$select_query = $select_query." $search_string $group_by";
+
 		$postscollection = $objThread->selectAll($select_query);
 		$totalFiltered   = count($postscollection);
-		$filter_query    = $select_query." $search_string $orderby limit $limit OFFSET $start ";
+		$filter_query    = $select_query." $search_string  $orderby limit $limit OFFSET $start ";
 		$postscollection = $objThread->selectAll($filter_query);
 		$data            = array();
 		foreach ($postscollection as $key => $item) {
@@ -72,6 +83,7 @@ class Home {
 			$row['name']          = '<a href="threadpost.php?id='.$item['id'].'">'.$item['title'].'</a>';
 			$action_array         = array();
 			$row['category_name'] = $item['category_name'];
+			$row['description']   = stripslashes($item['description']);
 
 			$row['action'] = $objThread->action($action_array, $this->twig);
 
