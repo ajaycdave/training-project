7ab9d49edd8207c813934aa0680385992bb9057c
ajayd <ajayd@aum.bz>
2021-02-22 17:48:40 +0530

    Thread changes add new method

--- threadall.php
+++ threadall.php
@@ -22,6 +22,9 @@ class Threadpage {
 	public function index() {
 		echo $this->twig->render('thread/thread_all.html.twig');
 	}
+	public function indexAbc() {
+		echo $this->twig->render('threadabc/thread_all.html.twig');
+	}
 	public function datalisting() {
 		$columns = array(
 			0=> 'id',
